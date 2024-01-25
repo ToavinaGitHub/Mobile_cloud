1280c99c9338201a64800fda69712a2fa6f0097f
Zak <118508607+ToavinaGitHub@users.noreply.github.com>
2024-01-25 06:35:08 +0300

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mobile_cloud_',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
